274b3da763460dfaf7910b39c446f53128286509
omer canpolat <114859630+omercanpolat@users.noreply.github.com>
2023-03-13 14:22:03 +0300

    first upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import TodoList from "./components/TodoList";
+
+const todoList = [
+  {
+    id: 1,
+    text: "React ile todolist uygulaması",
+    completed: false,
+  },
+  {
+    id: new Date().getTime(),
+    text: "uuid kullanımı",
+    completed: false,
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState(todoList);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header setTodos={setTodos} todos={todos} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { Button, InputGroup, Form } from "react-bootstrap";
+import { v4 as uuidv4 } from "uuid";
+
+const Header = ({ todos, setTodos }) => {
+  const [task, setTask] = useState("");
+  const addTodo = () => {
+    console.log(task);
+
+    const newTodo = {
+      id: uuidv4(),
+      text: task,
+      completed: false,
+    };
+    console.log(newTodo);
+    setTodos([...todos, newTodo]);
+    setTask("");
+  };
+
+  return (
+    <div>
+      <h1 className="text-center text-danger m-3">Todo App</h1>
+      <InputGroup className="mb-3 w-50 d-flex mx-auto">
+        <Form.Control
+          placeholder="Enter new todo..."
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <Button
+          className="input-group-text bg-succes"
+          disabled={!task.trim()}
+          id="basic-addon2"
+          onClick={addTodo}
+        >
+          Add Todo
+        </Button>
+      </InputGroup>
+    </div>
+  );
+};
+
+export default Header;
